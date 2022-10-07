1bc1e85f67927b2281ee45f698d42fa99cf95650
Alberto <acruz@alea-soluciones.com>
2022-10-07 08:03:54 +0200

    [DEV] Use common setup_venv script

--- dev/setup_venv.sh
+++ dev/setup_venv.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-if [ -z "$VIRTUAL_ENV" ]; then
-    echo "You need to be on a virtual environment to install dev dependencies"
-else
-    python -m pip install --upgrade pip wheel
-
-    python -m pip install --upgrade -r requirements-versioned.txt
-    python -m pip install --upgrade -r requirements-git.txt
-    python -m pip install --upgrade -r requirements-dev.txt
-
-    python -m pip install --upgrade -e .
-fi
+curl -s https://raw.githubusercontent.com/aleasoluciones/pydevlib/master/scripts/setup_venv.sh | bash
